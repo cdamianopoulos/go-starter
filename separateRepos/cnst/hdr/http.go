489c1b9708b15e969d3dc87a53bbfe570357da9e
Cam <91109601+Cam-asm@users.noreply.github.com>
2021-11-18 22:56:46 +1100

    Add HTTP Headers.

--- separateRepos/cnst/hdr/http.go
+++ separateRepos/cnst/hdr/http.go
@@ -1,9 +1,12 @@
-// Package hdr contains a list of HTTP header constants.
+// Package hdr provides a list of HTTP header constants. https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
 package hdr
 
 // TODO move cnst into a separate Git repository
 
 const (
-	Authorization = "Authorization"
-	ContentType   = "Content-Type"
+	Authorization           = "Authorization"
+	ContentSecurityPolicy   = "Content-Security-Policy"
+	ContentType             = "Content-Type"
+	StrictTransportSecurity = "Strict-Transport-Security"
+	UserAgent               = "User-Agent"
 )
---
+++ separateRepos/cnst/hdr/nonstandard.go
@@ -0,0 +1,5 @@
+package hdr
+
+const (
+	CorrelationID = "X-Correlation-ID"
+)
