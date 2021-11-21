d03a051399a59ea4d33da0ec9fcfc9519d67da11
Cam <91109601+Cam-asm@users.noreply.github.com>
2021-11-22 10:03:58 +1100

    Remove env global variable Add comments

--- cmd/main.go
+++ cmd/main.go
@@ -10,10 +10,12 @@ import (
 	"go-starter/separateRepos/utl"
 )
 
-var env api.Config
-
 func main() {
+	// Initialise logging.
 	logger.Init(api.Name)
+
+	// Load environment config from config.yml and environment variables.
+	var env api.Config
 	config.MustLoadYamlEnv("config.yml", api.EnvPrefix, &env)
 	env.Print()
 
