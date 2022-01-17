781f7eea875ab37c206089ef03b25649cb659d93
Con Damianopoulos <cdamianopoulos@assemblypayments.com>
2022-01-18 09:59:27 +1100

    remote debugginh set up

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"net/http"
 )
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/status", func (w http.ResponseWriter, r *http.Request) {
+
+	r.Use(middleware.DefaultLogger)
+	r.Get("/status", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		_, err := fmt.Fprint(w, `{"status": "healthy"}`)
 		if err != nil {
 			fmt.Printf("writing to stream: %v\n", err)
 		}
 	})
-	fmt.Println("Hello, serving on port 3000")
-	http.ListenAndServe(":3000", r)
+
+	fmt.Println("Hello, serving on port 3004")
+	http.ListenAndServe(":3004", r)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-starter
 
 go 1.16
 
-require github.com/go-chi/chi/v5 v5.0.4 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.4
+	github.com/google/gops v0.3.22 // indirect
+)
